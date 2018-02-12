d7b28dfa6d7b9e0cb01d7011021c2fa666302017
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:19:29 +0800

    chore: add NavDrawer component

---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+v-navigation-drawer(app dark fixed v-model="drawer")
+  v-list
+    v-list-tile(@click.native="gotoHome")
+      v-list-tile-action
+        v-icon home
+      v-list-tile-content
+        v-list-tile-title Home
+    v-list-tile(@click.native="reset")
+      v-list-tile-action
+        v-icon clear_all
+      v-list-tile-content
+        v-list-tile-title Reset
+    v-list-tile(@click.native="toggleFullscreen")
+      v-list-tile-action
+        v-icon fullscreen
+      v-list-tile-content
+        v-list-tile-title Fullscreen
+</template>
+
+<script>
+import EventBus from '@/EventBus'
+import {toggleFullscreen} from '@/utils'
+
+export default {
+  name: 'NavDrawer',
+  data: () => ({
+    drawer: null
+  }),
+  methods: {
+    toggleFullscreen () {
+      toggleFullscreen()
+      this.drawer = false
+    },
+    gotoHome () {
+      this.$router.push('/')
+      this.drawer = false
+    },
+    reset () {
+      localStorage.clear()
+      this.drawer = false
+      EventBus.$emit('toast', 'Reset done')
+    }
+  }
+}
+</script>
