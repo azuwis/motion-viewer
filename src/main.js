dacb7b4b122f66377297c73935ef4cc031ee177b
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:21:46 +0800

    chore: import material-design-icons and vuetify

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+import 'material-design-icons/iconfont/material-icons.css'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.grey.darken3,
+    secondary: colors.grey.lighten3,
+    accent: colors.indigo.base
+  }
+})
 
 new Vue({
   router,
