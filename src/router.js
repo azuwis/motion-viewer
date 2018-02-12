fee6fa10c3356131bf4f1c8233beffec9c4bbc33
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:51:34 +0800

    chore: router for MotionList

--- src/router.js
+++ src/router.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import MotionList from '@/components/MotionList.vue'
 
 Vue.use(Router)
 
+const motionViewerConfig = window.motionViewerConfig || {
+  liveStream: 'live/',
+  motionPrefix: 'motion/',
+  spriteFrames: 50
+}
+
 export default new Router({
   routes: [
+    {
+      path: '/:date',
+      name: 'date',
+      component: MotionList,
+      props: motionViewerConfig
+    }
   ]
 })
