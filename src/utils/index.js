e0f20e287c34852e760c637d042af910ad330c22
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:04:09 +0800

    chore: add utils

---
+++ src/utils/index.js
@@ -0,0 +1,40 @@
+export function getDate (date) {
+  return `${date.getFullYear()}-${padDate(date.getMonth() + 1)}-${padDate(date.getDate())}`
+}
+
+export function toggleFullscreen () {
+  if (document.fullscreenElement ||
+      document.mozFullScreenElement ||
+      document.webkitFullscreenElement ||
+      document.msFullscreenElement) {
+    exitFullscreen()
+  } else {
+    requestFullscreen(document.documentElement)
+  }
+}
+
+function padDate (str) {
+  return String('00' + str).slice(-2)
+}
+
+function requestFullscreen (element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen()
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen()
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen()
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen()
+  }
+}
+
+function exitFullscreen () {
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen()
+  }
+}
