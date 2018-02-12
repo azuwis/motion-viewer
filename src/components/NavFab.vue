3e2e3cae15b407b0fe8b08f2771f20a2d31af9c4
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:03:00 +0800

    chore: add NavFab component

---
+++ src/components/NavFab.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+div
+  v-fab-transition
+    v-btn#nav-fab-top(
+      fab
+      color="primary" dark small
+      fixed bottom right
+      @click.native="goToTop"
+    )
+      v-icon keyboard_arrow_up
+  v-fab-transition
+    v-btn(
+      fab
+      color="primary" dark small
+      fixed bottom right
+      @click.native="goToBottom"
+    )
+      v-icon keyboard_arrow_down
+</template>
+
+<script>
+export default {
+  name: 'NavFab',
+  methods: {
+    goToBottom () {
+      window.scrollTo(0, document.body.scrollHeight)
+    },
+    goToTop () {
+      window.scrollTo(0, 0)
+    }
+  }
+}
+</script>
+
+<style>
+#nav-fab-top {
+  bottom: 64px;
+}
+</style>
