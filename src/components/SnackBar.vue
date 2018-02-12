9307d8a4844a462141e49ee97227a91a91ee34fa
Zhong Jianxin <azuwis@gmail.com>
2018-02-12 13:19:06 +0800

    chore: add Snackbar component

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-snackbar(v-model="snackbar") {{ message }}
+  v-btn(
+    flat color="secondary"
+    @click.native="snackbar = false"
+  ) Dismiss
+</template>
+
+<script>
+import EventBus from '@/EventBus'
+
+export default {
+  name: 'Snackbar',
+  data: () => ({
+    message: null,
+    snackbar: null
+  }),
+  mounted () {
+    EventBus.$on('toast', message => {
+      this.message = message
+      this.snackbar = true
+    })
+  }
+}
+</script>
